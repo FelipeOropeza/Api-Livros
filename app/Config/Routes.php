671f69c1c686e16e856953cbf8a6e15b15fd5951
FelipeOropeza <felipe2006.co@gmail.com>
2025-04-16 22:10:52 -0300

    feat: acrecentar import correto

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,5 +5,5 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Livro::index');
+$routes->get('/', 'LivroController::index');
 $routes->post('categoria', 'CategoriaController::create');

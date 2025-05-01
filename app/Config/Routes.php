9136022f78378304ce20ff2306314a00720928e9
FelipeOropeza <felipe2006.co@gmail.com>
2025-05-01 19:39:10 -0300

    feat: aplicando cors dentro da api

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,10 +5,17 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'LivroController::index');
-$routes->post('livro', 'LivroController::create', ['filter' => 'auth']);
-$routes->get('livro', 'LivroController::getAllBook');
-$routes->post('categoria', 'CategoriaController::create', ['filter' => 'auth']);
-$routes->get('categoria', 'CategoriaController::getAllCategory');
 
-$routes->post('token', 'TokenController::generateToken');
+$routes->group('', ['filter' => 'cors'], static function (RouteCollection $routes): void {
+    $routes->get('/', 'LivroController::index');
+    $routes->post('livro', 'LivroController::create', ['filter' => 'auth']);
+    $routes->get('livro', 'LivroController::getAllBook');
+    $routes->post('categoria', 'CategoriaController::create', ['filter' => 'auth']);
+    $routes->get('categoria', 'CategoriaController::getAllCategory');
+    $routes->post('token', 'TokenController::generateToken');
+
+    $routes->options('livro', static fn () => null);
+    $routes->options('livro/(:any)', static fn () => null);
+    $routes->options('categoria', static fn () => null);
+    $routes->options('categoria/(:any)', static fn () => null);
+});
